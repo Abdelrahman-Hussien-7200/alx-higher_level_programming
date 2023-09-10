43c75b0758a40031f0af77d0e609c29543caa828
Abdelrahman-Hussien-7200 <abdhus5472@gmail.com>
2023-09-10 13:21:42 -0700

    abdo

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
-    new_string = my_string.remove('c', 'C')
+    new_string = ''
+    for i in my_string:
+        if i != 'c' and i != 'C':
+            new_string += i
     return (new_string)
