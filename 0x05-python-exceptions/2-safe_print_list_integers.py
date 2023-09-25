36678964e4d64ad378c7803ad7ec85966f9ef1d3
Abdelrahman-Hussien-7200 <abdhus5472@gmail.com>
2023-09-25 12:49:02 -0700

    abdo

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    number_of_elements = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+             number_of_elements += 1
+        except (ValueError, TypeError):
+            pass
+    print()
+    return  number_of_elements
