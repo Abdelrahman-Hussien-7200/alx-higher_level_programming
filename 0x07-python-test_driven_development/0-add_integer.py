3dd274f3fe71ca0def09079f996d35175488dfb3
Abdelrahman-Hussien-7200 <abdhus5472@gmail.com>
2023-10-01 12:07:09 -0700

    ando

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""
+function thta return the sum of two numbers a, b
+"""
+
+def add_integer(a, b=98):
+
+    """
+    add_integer: add two numbers
+    paramters:
+        a - integer number
+        b - integer number
+    Return:
+        sum of a and b
+    """
+    if type(a) not in [int, float]:
+        raise TypeError ("a must be an integer")
+    if type(b) not in [int, float]:
+        raise TypeError ("b must be an integer")
+    return int(a) + int(b)
