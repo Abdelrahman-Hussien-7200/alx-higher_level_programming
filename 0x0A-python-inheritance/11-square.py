df8490565301dc58e2250660e672ddc3eb7a1d99
Abdelrahman-Hussien-7200 <abdhus5472@gmail.com>
2023-10-09 21:22:33 +0000

    abdo

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+class square that inherits from rectangle
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+"""
+class Square that inherits from Rectangle
+"""
+
+
+class Square(Rectangle):
+    """
+    subsubclass square
+    """
+    def __init__(self, size):
+        super().integer_validator("size", size)
+        self.__size = size
+        Rectangle.__init__(self, size, size)
+
+    def area(self):
+        return self.__size * self.__size
+
+    def __str__(self):
+        return f"[Square] {self.__width}/{self.__height}"
