36153aa607d128de91450046b660aa57cf06aa28
Abdelrahman-Hussien-7200 <abdhus5472@gmail.com>
2023-10-10 22:39:00 +0200

    abdo

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
-"""Defines a class Student."""
+"""
+Write a class Student that defines a student by:
+Public instance attributes:first_name, last_name
+,age
+"""
 
 
 class Student:
-    """Represent a student."""
+    """class a student."""
 
     def __init__(self, first_name, last_name, age):
-        """Initialize a new Student.
-
-        Args:
-            first_name (str): The first name of the student.
-            last_name (str): The last name of the student.
-            age (int): The age of the student.
+        """
+            Initialize the class Student.
+            first_name (str): The first name for student.
+            last_name (str): The last name for student.
+            age (int): student age.
         """
         self.first_name = first_name
         self.last_name = last_name
