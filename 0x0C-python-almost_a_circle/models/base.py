6745f939fae43d2856bd955865107e96e0b30ad5
Abdelrahman-Hussien-7200 <abdhus5472@gmail.com>
2023-10-14 04:26:39 -0700

    abdo

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+Class Base:
+private class attribute __nb_objects = 0
+class constructor: def __init__(self, id=None):
+if id is not None, assign the public instance attribute
+id with this argument value - you can
+assume id is an integer and you don’t need to test the type of it
+otherwise, increment __nb_objects and assign the
+new value to the public instance attribute id
+"""
+
+
+Class Base():
+    """
+    base calss
+    """
+    __nb_objects = 0
+    def __init__(self, id=None):
+        """
+        init class constructor
+        """
+        if id != None:
+            id = None
+    __nb_objects += 1
+
