6812222354209e20105d34071893b1e58680e17f
Abdelrahman-Hussien-7200 <abdhus5472@gmail.com>
2023-10-14 11:18:23 -0700

    abdo

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -55,3 +55,14 @@ Class Square(Rectangle):
             for key, value in kwargs.items():
                 if hasattr(self, key):
                     setattr(self, key, value)
+
+    def to_dictionary(self):
+        """
+        returns the dictionary representation of a Square
+        """
+        return {
+            "id": self.id,
+            "size": self.size,
+            "x": self.x,
+            "y": self.y
+        }
