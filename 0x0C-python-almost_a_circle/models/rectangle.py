b2fd1c4d25918f3eb17af63a17843835c312b980
Abdelrahman-Hussien-7200 <abdhus5472@gmail.com>
2023-10-14 10:32:58 -0700

    abdo

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -110,10 +110,10 @@ class Rectangle(Base):
         print(rectangle, end="")
 
     def __str__(self):
-         """
-         overriding the __str__ method so that it returns
-         [Rectangle] (<id>) <x>/<y> - <width>/<height>
-         """
+        """
+        overriding the __str__ method so that it returns
+        [Rectangle] (<id>) <x>/<y> - <width>/<height>
+        """
         return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x, self.y,
                                                        self.width, self.height)
 
