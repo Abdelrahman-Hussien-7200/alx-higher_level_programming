0e2c597265835fd852d55bb91599945ed2fde04f
Abdelrahman-Hussien-7200 <abdhus5472@gmail.com>
2023-10-15 11:47:31 -0700

    abdo

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -45,3 +45,12 @@ Class Base:
             else:
                 list_dicts = [obj.to_dictionary() for obj in list_objs]
                 abdofile.write(cls.to_json_string(list_dicts))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """
+        returns the list of the JSON string representation json_string
+        """
+        if json_string is None or json_string == "[]":
+            return []
+        return json.loads(json_string)
