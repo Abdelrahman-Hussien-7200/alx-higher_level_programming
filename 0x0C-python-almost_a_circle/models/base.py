ddd536cbe562ae87adab96e4f0d6c87da5fbd0d2
Abdelrahman-Hussien-7200 <abdhus5472@gmail.com>
2023-10-15 11:44:17 -0700

    abdo

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,13 +1,6 @@
 #!/usr/bin/python3
 """
-Class Base:
-private class attribute __nb_objects = 0
-class constructor: def __init__(self, id=None):
-if id is not None, assign the public instance attribute
-id with this argument value - you can
-assume id is an integer and you don’t need to test the type of it
-otherwise, increment __nb_objects and assign the
-new value to the public instance attribute id
+Base class and methods
 """
 import json
 
@@ -38,3 +31,17 @@ Class Base:
         if list_dictionaries is None or list_dictionaries == []:
             return []
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """
+        writes the JSON string representation
+        of list_objs to a file
+        """
+        filename = cls.__name__ + ".json"
+        with open(filename, "w") as abdofile:
+            if list_objs is None:
+                abdofile.write("[]")
+            else:
+                list_dicts = [obj.to_dictionary() for obj in list_objs]
+                abdofile.write(cls.to_json_string(list_dicts))
