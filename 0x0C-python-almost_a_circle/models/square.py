3da79a7ca467f41140102b3491ac0d71d4f6e780
Abdelrahman-Hussien-7200 <abdhus5472@gmail.com>
2023-10-17 21:43:57 +0200

    abdo

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,42 +1,52 @@
 #!/usr/bin/python3
-"""Defines a square class."""
+"""
+class Square that inherits from Rectangle
+"""
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """Represent a square."""
+    """
+    class Square
+    """
 
     def __init__(self, size, x=0, y=0, id=None):
-        """Initialize a new Square.
+        """
+        Class constructor
 
         Args:
-            size (int): The size of the new Square.
-            x (int): The x coordinate of the new Square.
-            y (int): The y coordinate of the new Square.
-            id (int): The identity of the new Square.
+            size (int): The size Square.
+            x (int): The x Square.
+            y (int): The y Square.
+            id (int): The identity Square.
         """
         super().__init__(size, size, x, y, id)
 
     @property
     def size(self):
-        """Get/set the size of the Square."""
+        """
+        getter width or height = size
+        """
         return self.width
 
     @size.setter
     def size(self, value):
+        """
+        setter for Square height and width
+        """
         self.width = value
         self.height = value
 
     def update(self, *args, **kwargs):
-        """Update the Square.
-
+       """
+        update square props
         Args:
             *args (ints): New attribute values.
-                - 1st argument represents id attribute
-                - 2nd argument represents size attribute
-                - 3rd argument represents x attribute
-                - 4th argument represents y attribute
-            **kwargs (dict): New key/value pairs of attributes.
+                - 1st argument id
+                - 2nd argument size
+                - 3rd argument x
+                - 4th argument y
+            **kwargs (dict): New key/value pairs
         """
         if args and len(args) != 0:
             a = 0
@@ -69,7 +79,9 @@ class Square(Rectangle):
                     self.y = v
 
     def to_dictionary(self):
-        """Return the dictionary representation of the Square."""
+        """
+        returns the dictionary representation of a Square
+        """
         return {
             "id": self.id,
             "size": self.width,
@@ -78,6 +90,8 @@ class Square(Rectangle):
         }
 
     def __str__(self):
-        """Return the print() and str() representation of a Square."""
+        """
+        Square readable output
+        """
         return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
                                                  self.width)
